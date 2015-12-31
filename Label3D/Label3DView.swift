c7bac469bfe32611cd206e6ee0eded9e4bfe8900
flybywind@foxmail.com <flybywind@foxmail.com>
2015-12-31 10:39:16 +0800

    尝试增加scroll y效果。

--- Label3D/Label3DView.swift
+++ Label3D/Label3DView.swift
@@ -54,12 +54,12 @@ public class Label3DView: UIView {
             label.font = UIFont.systemFontOfSize(fontSize)
             label.textColor = fontColor
             label.sizeToFit()
-            let rx = RandomFloat(min: 0, max: PI_2)
+            let rxz = RandomFloat(min: 0, max: PI_2)
             let ry = RandomFloat(min: 0, max: PI_2)
-            let rz = RandomFloat(min: 0, max: PI_2)
-            label.rx = rx
+//            let rz = RandomFloat(min: 0, max: PI_2)
+            label.rxz = rxz
             label.ry = ry
-            label.rz = rz
+//            label.rz = rz
             label.cx = cx
             label.cy = cy
             label.radius = sphereRadius*perspective
@@ -77,21 +77,20 @@ public class Label3DView: UIView {
         return (d_rx, d_ry)
     }
     func handelPan(gesture: UIPanGestureRecognizer) {
-        let (d_rx, d_ry) = panGestureCallback(gesture)
+        let (d_rx, _) = panGestureCallback(gesture)
         scrollX(d_rx)
-        scrollY(d_ry)
+//        scrollY(d_ry)
     }
-
+    // 绕着y轴转，和xz平面的夹角变，但是ry不变
     func scrollX(th:Float) {
         for label in titles {
-            label.rx -= th
-            label.rz += th
+            label.rxz += th
         }
     }
+    // TODO: 绕着x轴转，和yz平面的夹角变，但是rx不变
     func scrollY(th:Float) {
         for label in titles {
-            label.ry -= th
-            label.rz += th
+            label.ryz += th
         }
     }
 }
@@ -101,49 +100,57 @@ class LabelSphere: UILabel {
     
     var cx:Float {
         didSet {
-            self.layer.position.x = CGFloat(cos(rx) * radius + cx)
+            setXZ()
         }
     }
     var cy:Float {
         didSet {
-            self.layer.position.y = CGFloat(cos(ry) * radius + cy)
+            setY()
+        }
+    }
+    // 半径在xz平面上的投影和z轴之间的夹角
+    var rxz:Float {
+        didSet {
+            setXZ()
         }
     }
     
-    var rx:Float {
-        didSet {
-            self.layer.position.x = CGFloat(cos(rx) * radius + cx)
-        }
-    }
     var ry:Float {
         didSet{
-            self.layer.position.y = CGFloat(cos(ry) * radius + cy)
+            setXZ()
+            setY()
         }
     }
-    var rz:Float {
-        didSet{
-            let pz = CGFloat(cos(rz)*radius)
-            let old_pz = CGFloat(cos(oldValue)*radius)
-            self.layer.zPosition = pz
-            
-            self.setTransform3D(pz - old_pz)
-            self.alpha = self.getAlpha()
-        }
-    }
+
     var radius:Float {
         didSet{
-            self.layer.position.x = CGFloat(cos(rx) * radius + cx)
-            self.layer.position.y = CGFloat(cos(ry) * radius + cy)
-
-            let pz = CGFloat(cos(rz)*radius)
-            let old_pz = CGFloat(cos(rz)*oldValue)
-            self.layer.zPosition = pz
-            
-            self.setTransform3D(pz - old_pz)
+            setXZ()
+            setY()
             self.alpha = self.getAlpha()
         }
     }
-    
+    // 辅助属性：
+    // 半径在yz平面上的投影和z轴之间的夹角
+    var ryz:Float {
+        get {
+            let (_, py, pz) = getXYZ()
+            return atan(py/pz)
+        }
+        set {
+            let pyz = sin(rx) * radius
+            let py = sin(ryz) * pyz
+            let pz = cos(ryz) * pyz
+            let px = cos(rx) * radius
+            ry = asin(py/radius)
+            rxz = atan(px/pz)
+        }
+    }
+    var rx:Float {
+        get {
+            let (px, _, _) = getXYZ()
+            return acos(px/radius)
+        }
+    }
     var perspective:Float {
         didSet {
             self.layer.transform.m34 = CGFloat(-1.0/perspective)
@@ -156,9 +163,8 @@ class LabelSphere: UILabel {
         self.text = text
     }
     override init(frame: CGRect) {
-        rx = 0
+        rxz = 0
         ry = 0
-        rz = 0
         cx = 0
         cy = 0
         radius = 0
@@ -167,9 +173,8 @@ class LabelSphere: UILabel {
     }
     
     required init?(coder aDecoder: NSCoder) {
-        rx = aDecoder.decodeFloatForKey("rx")
+        rxz = aDecoder.decodeFloatForKey("rxz")
         ry = aDecoder.decodeFloatForKey("ry")
-        rz = aDecoder.decodeFloatForKey("rz")
         cx = aDecoder.decodeFloatForKey("cx")
         cy = aDecoder.decodeFloatForKey("cy")
         radius = aDecoder.decodeFloatForKey("radius")
@@ -177,6 +182,26 @@ class LabelSphere: UILabel {
         super.init(coder: aDecoder)
     }
     
+    func setXZ() {
+        let pxz = sin(ry) * radius
+        self.layer.position.x = CGFloat(sin(rxz) * pxz + cx)
+        let old_pz = self.layer.zPosition
+        self.layer.zPosition = CGFloat(cos(rxz) * pxz)
+        setTransform3D(self.layer.zPosition - old_pz)
+        self.alpha = self.getAlpha()
+    }
+    
+    func getXYZ() -> (Float, Float, Float) {
+        let pxz = sin(ry) * radius
+        let px = sin(rxz) * pxz
+        let pz = cos(rxz) * pxz
+        
+        return (px, cos(ry)*radius, pz)
+    }
+    
+    func setY() {
+        self.layer.position.y = CGFloat(cos(ry) * radius + cy)
+    }
     func setTransform3D(dz:CGFloat) {
         let t = self.layer.transform
         self.layer.transform = CATransform3DTranslate(t, 0, 0, dz)
@@ -184,7 +209,7 @@ class LabelSphere: UILabel {
     func getAlpha() -> CGFloat {
         var alpha = 2*self.layer.zPosition/CGFloat(perspective) + 0.5
         alpha = min(1.0, alpha)
-        alpha = max(0, alpha)
+        alpha = max(0.1, alpha)
         return alpha
     }
 }
